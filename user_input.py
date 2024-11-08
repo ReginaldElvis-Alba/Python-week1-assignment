150b90997de37851e6beafba6c23edaa3ec8a87a
Reginald <elvisreginald1@gmail.com>
2024-11-09 00:24:16 +0300

    user_inputs

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#User name to be provided
+name=input("What is your name:?")
+
+#User to provide their age
+age=int(input("What is your age:?"))
+
+#User to provide their location 
+location=input("Where do you come from:?")
+
+#Printing out a personalized message
+print(f"Hello! my name is {name} and i am {age} years old.I hail from the city of {location}")
